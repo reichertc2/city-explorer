e2ca8a952e299376a0a2cdf490b1f9542ff6a01d
Chris Reichert <chris.a.reichert@gmail.com>
2021-10-06 21:39:26 -0500

    completed storyboard 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import './App.css';
 import React from 'react';
+import axios from 'axios';
+import { ThemeConsumer } from 'react-bootstrap/esm/ThemeProvider';
 
 class App extends React.Component {
-  render(){
-  return (
-    <>
-    <h1>Here i am</h1>
-    </>
-  )};
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityEntry: '',
+      locationQuery: {}
+    }
+  }
+  getLocation = async () => {
+    // .preventDefault();
+    console.log('Exploration Initated');
+    let REACT_APP_LOCATION_API_KEY = process.env.REACT_APP_LOCATION_API_KEY
+    let apiUrl = `https://us1.locationiq.com/v1/search.php?key=${REACT_APP_LOCATION_API_KEY}&q=${this.state.cityEntry}&format=json`
+    console.log(apiUrl);
+    let dataPull = await axios.get(apiUrl);
+    console.log(dataPull.data[0]);
+    this.setState({ locationQuery: dataPull.data[0] })
+  }
+  render() {
+    return (
+      <>
+        <h1>Here i am</h1>
+        <h4>{this.state.cityEntry}</h4>
+
+        <input onChange={(event) => {
+          event.preventDefault();
+          this.setState({ cityEntry: event.target.value })
+        }} placeholder="Enter City Name"></input>
+
+        <button onClick={this.getLocation}>Explore!!</button>
+        <h3>{this.state.locationQuery.display_name}</h3>
+        <h4>Latitude: {this.state.locationQuery.lat}</h4>
+        <h4>Longitude: {this.state.locationQuery.lon}</h4>
+
+
+
+      </>
+    )
+  };
 }
 
 export default App;
