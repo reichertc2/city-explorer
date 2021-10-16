3cd294840e23ada5171474e6e60382aac19b0ec2
Chris Reichert <chris.a.reichert@gmail.com>
2021-10-16 15:22:03 -0500

    updated heroku error

--- src/App.js
+++ src/App.js
@@ -58,8 +58,8 @@ class App extends React.Component {
       }
       // let weatherAPI = await axios.get(`http://localhost:3001/weather?lat=${params.lat}&lon=${params.lon}&searchQuery=${this.state.cityEntry}`);
       // let movieAPI = await axios.get(`http://localhost:3001/movies?searchQuery=${this.state.cityEntry}`)
-      let weatherAPI = await axios.get(`https://fierce-badlands-59125.herokuapp.com//weather?lat=${params.lat}&lon=${params.lon}&searchQuery=${this.state.cityEntry}`);
-      let movieAPI = await axios.get(`https://fierce-badlands-59125.herokuapp.com//movies?searchQuery=${this.state.cityEntry}`)
+      let weatherAPI = await axios.get(`https://fierce-badlands-59125.herokuapp.com/weather?lat=${params.lat}&lon=${params.lon}&searchQuery=${this.state.cityEntry}`);
+      let movieAPI = await axios.get(`https://fierce-badlands-59125.herokuapp.com/movies?searchQuery=${this.state.cityEntry}`)
       // console.log(weatherAPI.data);
       // console.log(movieAPI);
       this.setState({
